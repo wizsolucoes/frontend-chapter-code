30ea8dc532ec39175b357df9e72d6ef3d3cf89d7
Touré Holder <toureh@hotmail.com>
2021-06-29 17:50:41 -0300

    chore(making-presentational-components): copy ts html e css to presentational component

--- packages/making-presentational-components/src/app/shared/components/login-form/login-form.component.ts
+++ packages/making-presentational-components/src/app/shared/components/login-form/login-form.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
 })
-export class LoginFormComponent implements OnInit {
+export class LoginFormComponent {
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+  loginErrorMessage: string | undefined;
+  loginButtonMessage = 'Entrar';
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  get email(): AbstractControl | null {
+    return this.form.get('email');
   }
 
+  get password(): AbstractControl | null {
+    return this.form.get('password');
+  }
+
+  onSubmit(): void {
+    if (this.form.invalid) {
+      return;
+    }
+    this.loginButtonMessage = 'Entrando';
+  }
 }
--- packages/making-presentational-components/src/app/shared/components/login-form/login-form.module.ts
+++ packages/making-presentational-components/src/app/shared/components/login-form/login-form.module.ts
@@ -1,15 +1,11 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './login-form.component';
-
-
+import { SharedModule } from '../../shared.module';
 
 @NgModule({
-  declarations: [
-    LoginFormComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoginFormComponent],
+  imports: [CommonModule, SharedModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class LoginFormModule { }
+export class LoginFormModule {}
