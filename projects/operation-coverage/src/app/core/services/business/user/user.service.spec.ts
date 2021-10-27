f485d5c042f6f22aaa2f6795675564c5351d92a6
Touré Holder <toureh@hotmail.com>
2021-10-27 14:11:36 -0300

    chore(operation-coverage): focus on user service test and mock http client

--- projects/operation-coverage/src/app/core/services/business/user/user.service.spec.ts
+++ projects/operation-coverage/src/app/core/services/business/user/user.service.spec.ts
@@ -1,12 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
 
-describe('UserService', () => {
+fdescribe('UserService', () => {
   let service: UserService;
+  let mockHttpClient: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockHttpClient = jasmine.createSpyObj('mockHttpClient', ['get']);
+
+    TestBed.configureTestingModule({
+      providers: [{ provide: HttpClient, useValue: mockHttpClient }],
+    });
+
     service = TestBed.inject(UserService);
   });
 
